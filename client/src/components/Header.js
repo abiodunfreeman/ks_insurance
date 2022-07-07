ef9b59d753a3d96f58a09c99c90c61672fd556a0
Odun Freeman <bornduringwar@gmail.com>
2022-07-06 20:05:56 -0400

    working mobile nav with hamburger icon and onclick function set up

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,48 @@
 import './styles/css/header.css';
 import logo from './images/logo/profile.png';
+import { useState } from 'react';
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleMenuClick = () => {
+    console.log('menu click');
+    const menuBtn = document.querySelector('.menu-btn');
+    const headerMobileNav = document.querySelector('#mobile-header-nav');
+    if (!menuOpen) {
+      menuBtn.classList.add('open');
+      headerMobileNav.classList.add('active-nav');
+      setMenuOpen(prevState => !prevState);
+    } else if (menuOpen) {
+      menuBtn.classList.remove('open');
+      headerMobileNav.classList.remove('active-nav');
+      setMenuOpen(prevState => !prevState);
+    }
+  };
   return (
     <header>
-      <div>
+      <div id="header-nav">
         <a className="link link-primary">Services</a>
         <a className="link link-info">About</a>
         <a className="link link-info">Contact Us</a>
       </div>
 
-      <img src={logo} alt="logo" />
-
-      <a href="/quote" className="btn btn-primary">
+      <img src={logo} alt="logo" id="header-logo" />
+      <div onClick={() => handleMenuClick()} className="menu-btn">
+        <div className="menu-btn__burger"></div>
+      </div>
+      <a href="/quote" id="header-btn" className="btn btn-primary ">
         Get a Quote
       </a>
+      <nav id="mobile-header-nav">
+        <div className="link-list">
+          <a className="link link-primary">Services</a>
+          <a className="link link-info">About</a>
+          <a className="link link-info">Contact Us</a>
+        </div>
+
+        <a className="btn btn-primary" href="/quote">
+          Get a Quote
+        </a>
+      </nav>
     </header>
   );
 };
