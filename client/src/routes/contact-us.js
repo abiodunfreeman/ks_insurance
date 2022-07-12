3334e8088b50b2a1a6e7acd8767aa502b8793465
Odun Freeman <bornduringwar@gmail.com>
2022-07-12 19:40:40 -0400

    contact-us page complete enough i think

--- client/src/routes/contact-us.js
+++ client/src/routes/contact-us.js
@@ -7,7 +7,32 @@ export default function ContactUs() {
     <div id="contact-route-container">
       <Header />
       <div className="pad-10"></div>
-      <div id="contact-form-container">
+      <div id="contact-form-container" className="gap-8">
+        <div className="contact-info-container">
+          <h1>Contact KS Insurance Agency</h1>
+          <section className="flex flex-wrap justify-between  ">
+            <p>
+              <span className="font-bold text-red-500">Phone: </span>(440)
+              354-4664
+            </p>
+            <p>
+              <span className="font-bold text-red-500">Adress: </span>78 Mentor
+              Ave Painesville, OH 44077
+            </p>
+            <p>
+              <span className="font-bold text-red-500">Fax: </span>(440)
+              354-4664
+            </p>
+            <p>
+              <span className="font-bold text-red-500">Email: </span>
+              ks.insur.agency@gmail.com
+            </p>
+          </section>
+          <h5>
+            Our team of experienced professionals are eager to lend a hand.{' '}
+            <br /> Get in touch today.
+          </h5>
+        </div>
         <ContactForm />
       </div>
       <Footer />
