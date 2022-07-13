7236a81ef1c431bdc1be4d8978d4612435193309
Odun Freeman <bornduringwar@gmail.com>
2022-07-13 18:41:22 -0400

    css

--- client/src/routes/quote.js
+++ client/src/routes/quote.js
@@ -5,36 +5,39 @@ export default function Quote() {
   return (
     <section id="quote-homepage">
       <Header />
-      <div id="quote-form-container">
-        <form>
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            id="quote-form-name"
-          />
-          <input
-            type="text"
-            placeholder="Email Address"
-            name="email"
-            id="quote-form-email"
-          />
-          <input
-            type="text"
-            placeholder="Phone Number"
-            name="phone"
-            id="quote-form-phone"
-          />
-          <textarea
-            placeholder="Message"
-            name="message"
-            id="quote-form-message"
-          />
-          <button className="btn btn-primary" type="submit">
-            SUBMIT
-          </button>
-        </form>
+      <div className=" bg-blue-100">
+        <div id="quote-form-container">
+          <form>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              id="quote-form-name"
+            />
+            <input
+              type="text"
+              placeholder="Email Address"
+              name="email"
+              id="quote-form-email"
+            />
+            <input
+              type="text"
+              placeholder="Phone Number"
+              name="phone"
+              id="quote-form-phone"
+            />
+            <textarea
+              placeholder="Message"
+              name="message"
+              id="quote-form-message"
+            />
+            <button className="btn btn-primary" type="submit">
+              SUBMIT
+            </button>
+          </form>
+        </div>
       </div>
+
       <Footer />
     </section>
   );
